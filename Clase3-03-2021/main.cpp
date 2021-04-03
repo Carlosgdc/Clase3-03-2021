09a62c19c7ca7c825a81814f255b4073ce85264c
Carlosgdc <cdiazca@unal.edu.co>
2021-04-02 21:01:42 -0500

    Initial Commit

---
+++ Clase3-03-2021/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Clase3-03-2021
+//
+//  Created by Countyfoot on 4/2/21.
+//
+// Lo que haremos en esta clase consistira en calcular los numeros primos desde 1 hasta 100.
+// Comenzaremos incluyendo las librerias.
+
+#include <iostream>
+
+int main(void)
+{
+    // insert code here...
+
+    return 0;
+}
