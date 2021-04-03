f9df9ffa0fe141a9f2d2cd2a9a332907247c628a
Carlosgdc <cdiazca@unal.edu.co>
2021-04-02 21:54:52 -0500

    Ahora nuestro objetivo consiste en enristra los números pares desde el 1 al 100, al utilizar un condicional para expresar solo ciertos números.

--- Clase3-03-2021/main.cpp
+++ Clase3-03-2021/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by Countyfoot on 4/2/21.
 //
-// Lo que haremos en esta clase consistira en calcular los numeros primos desde 1 hasta 100.
+// Lo que haremos en esta clase consistira en calcular los numeros pares desde 1 hasta 100.
 // Comenzaremos incluyendo las librerias.
 
 #include <iostream>
@@ -21,7 +21,11 @@ int main(void)
     //Añadimos un bucle en donde se repitan una instrucciones para muchos datos, utilizaremos la función For.
     for(int ii = m; ii <= n; ii = ii+1 )
     {
-        std::cout << ii << "\n";
+        //Añadiremos un condicional para ir "condicionando" los numeros que nos vayan apareciendo. Asi si el residuo es 0 al dividir por dos, el numero sera impreso.
+        if (ii%2 == 0)
+        {
+            std::cout << ii << "\n";
+        }
     }
     return 0;
 }
